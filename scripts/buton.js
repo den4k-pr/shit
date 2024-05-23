02dbc5669545180949accb29d5b2e443b3b7238f
Denys Yakovenko <106625230+den4k-pr@users.noreply.github.com>
2024-05-23 18:33:00 +0200

    Add files via upload

---
+++ scripts/buton.js
@@ -0,0 +1,7 @@
+document.querySelector(".footerButtonUp").addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+})
+
---
+++ scripts/slider.js
@@ -0,0 +1,51 @@
+const imagePaths2 = [
+    "./images/products/product-2-1.jpg",
+    "./images/products/product-2-2.jpg",
+    "./images/products/product-2-3.jpg",
+    "./images/products/product-2-4.jpg",
+    "./images/products/product-2-5.jpg",
+    "./images/products/product-2-6.jpg",
+    "./images/products/product-2-7.jpg",
+    "./images/products/product-2-8.jpg",
+    "./images/products/product-2-9.jpg"
+];
+
+const prevButton2 = document.getElementById('prev2');
+const nextButton2 = document.getElementById('next2');
+const sliderImage2 = document.getElementById('sliderImage2');
+const sliderNextImage2 = document.getElementById('sliderNextImage2');
+let currentIndex2 = 0;
+
+// Preload images
+const preloadedImages2 = [];
+imagePaths2.forEach((path) => {
+    const img = new Image();
+    img.src = path;
+    preloadedImages2.push(img);
+});
+
+function showImage2(index) {
+    sliderImage2.src = preloadedImages2[index].src;
+}
+
+function showNextImage2(index) {
+    sliderNextImage2.src = preloadedImages2[index].src;
+}
+
+function updateImages() {
+    showImage2(currentIndex2)
+    const nextIndex2 = (currentIndex2 + 1) % preloadedImages2.length;
+    showNextImage2(nextIndex2)
+}
+
+prevButton2.addEventListener('click', () => {
+    currentIndex2 = (currentIndex2 > 0) ? currentIndex2 - 1 : preloadedImages2.length - 1;
+    updateImages();
+});
+
+nextButton2.addEventListener('click', () => {
+    currentIndex2 = (currentIndex2 < preloadedImages2.length - 1) ? currentIndex2 + 1 : 0;
+    updateImages();
+});
+
+updateImages();
